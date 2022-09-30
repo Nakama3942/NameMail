879d4543d0f88b00c7e7d65692a50304184014ca
Kalynovsky Valentin <nakama3942@gmail.com>
2022-09-30 18:28:07 +0300

    Added main window program

--- main.py
+++ main.py
@@ -2,30 +2,38 @@ from mail import MailSMTP, MailIMAP
 from smtp import *
 from config import *
 
+from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QApplication, QMainWindow
+import sys
 
-def TestSMTP():
-    with open('message.txt', 'r') as file:
-        message = file.read()
-
-    gmail = MailSMTP(SMTPHost.gmail.value, SMTPPort.gmail.value)
-    gmail.server_login(mail, password)
-    gmail.create_message(['valentynkalynovskyi@gmail.com',
-                          'dzonile.f.g.i@gmail.com',
-                          'kalinovskijvalentin@gmail.com'],
-                         'Just a test',
-                         message)
-    gmail.add_image('image.jpg')
-    gmail.send()
-    gmail.close()
-
-
-def TestIMAP():
-    imail = MailIMAP(SMTPHost.gmail.value)
-    imail.server_login(mail, password)
-    imail.get_message()
-    imail.close()
-
+from ui.namemail import Ui_NameMail
 
 if __name__ == "__main__":
-    TestSMTP()
-    # TestIMAP()
+    app = QApplication(sys.argv)
+    NameMail = QMainWindow()
+    ui = Ui_NameMail()
+    ui.setupUi(NameMail)
+    NameMail.show()
+    sys.exit(app.exec())
+
+# def TestSMTP():
+#     with open('message.txt', 'r') as file:
+#         message = file.read()
+#
+#     gmail = MailSMTP(SMTPHost.gmail.value, SMTPPort.gmail.value)
+#     gmail.server_login(mail, password)
+#     gmail.create_message(['valentynkalynovskyi@gmail.com',
+#                           'dzonile.f.g.i@gmail.com',
+#                           'kalinovskijvalentin@gmail.com'],
+#                          'Just a test',
+#                          message)
+#     gmail.add_image('image.jpg')
+#     gmail.send()
+#     gmail.close()
+#
+#
+# def TestIMAP():
+#     imail = MailIMAP(SMTPHost.gmail.value)
+#     imail.server_login(mail, password)
+#     imail.get_message()
+#     imail.close()
