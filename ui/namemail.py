541a700522e56aefd35aa8371e72cf44ff7210c4
Kalynovsky Valentin <nakama3942@gmail.com>
2022-10-01 13:41:45 +0300

    Construction of the frame of the main window

--- ui/namemail.py
+++ ui/namemail.py
@@ -12,13 +12,34 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from PyQt6 import QtWidgets, uic
+from PyQt6 import QtWidgets, QtCore, uic
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
 from ui.raw.ui_namemail import Ui_NameMail
+
+# Origin source:
+#   https://www.pythonguis.com/tutorials/first-steps-qt-creator/
+
+
+def buttSend_Released():
+    # todo Реалізувати вікно відправлення повідомлення
+    print("Not available! 111")
+
+
+def listLetters_Activated(aitem: QtWidgets.QListWidgetItem):
+    # todo Реалізувати вікно перегляду повідомлення
+    print("Not available! 222 + " + str(aitem.text()))
 
 
 class NameMail(QMainWindow, Ui_NameMail):
     def __init__(self):
         super(NameMail, self).__init__()
         self.setupUi(self)
+
+        # Об'єданння графічних елементів зі слотами
+        self.buttSend.released.connect(lambda: buttSend_Released())
+        self.listLetters.activated.connect(lambda: listLetters_Activated(self.listLetters.currentItem()))
+
+        # Формування списку повідомлень
+        for item in range(10):
+            self.listLetters.addItem("Item: " + str(item))
