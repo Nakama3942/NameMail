a9f01599a58f000d5325aa8528863050359cfa9b
Kalynovsky Valentin <nakama3942@gmail.com>
2022-10-04 22:24:12 +0300

    Fixed inaccurate work of multithreading

--- ui/namemail.py
+++ ui/namemail.py
@@ -16,6 +16,7 @@ from threading import Thread
 
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtCore import QThread, pyqtSignal
 
 from ui.raw.ui_namemail import Ui_NameMail
 from ui.reviewer import Reviewer
@@ -29,6 +30,23 @@ from src.config import mail_login, mail_password
 #   https://www.pythonguis.com/tutorials/first-steps-qt-creator/
 
 
+class ProgressThread(QThread):
+    update = pyqtSignal(int, int)
+
+    def __init__(self, get_mail: MailIMAP):
+        super(ProgressThread, self).__init__()
+        self.get_mail = get_mail
+
+    def run(self):
+        count_mails = len(self.get_mail.id_list) - 1
+        current = len(self.get_mail.messages)
+        while current <= count_mails:
+            if current < len(self.get_mail.messages):
+                self.update.emit(current := len(self.get_mail.messages), count_mails)
+            if current == count_mails:
+                break
+
+
 class NameMail(QMainWindow, Ui_NameMail):
     def __init__(self):
         super(NameMail, self).__init__()
@@ -39,7 +57,10 @@ class NameMail(QMainWindow, Ui_NameMail):
         self.message_subject: list[str] = []
 
         self.progressbar = QtWidgets.QProgressBar()
+        self.status = QtWidgets.QLabel()
+        self.status.setText("Hello (* ^ ω ^)")
         self.statusbar.addWidget(self.progressbar)
+        self.statusbar.addWidget(self.status)
 
         # Об'єданння графічних елементів зі слотами
         self.buttSend.released.connect(lambda: self.buttSend_Released())
@@ -53,23 +74,27 @@ class NameMail(QMainWindow, Ui_NameMail):
     def get_message(self, get_mail: MailIMAP):
         get_mail.server_login(mail_login, mail_password)
         get_mail.get_list()
-        progres_thread = Thread(target=self.progress_bar_reboot, args=(get_mail,))
-        progres_thread.start()
+
+        progress_thread = ProgressThread(get_mail)
+        progress_thread.start()
+        progress_thread.update.connect(self.update_status_bar)
+
         get_mail.get_messages()
         get_mail.close()
+        self.status.setText("Mail downloaded＼(≧▽≦)／")
+        count = 0
+        for item in get_mail.messages:
+            count += 1
+            item_from = str(email.header.make_header(email.header.decode_header(item['from'])))
+            item_subject = str(email.header.make_header(email.header.decode_header(item['subject'])))
+            self.listLetters.addItem(QtWidgets.QListWidgetItem(f"{count}\nFrom : {item_from}\nSubject : {item_subject}"))
+            self.message_from.append(item_from)
+            self.message_subject.append(item_subject)
 
-    def progress_bar_reboot(self, get_mail: MailIMAP):
-        self.progressbar.setMaximum(len(get_mail.id_list) - 1)
-        current_number_of_rocessed_list_id = len(get_mail.messages)
-        while current_number_of_rocessed_list_id < len(get_mail.id_list) - 1:
-            if current_number_of_rocessed_list_id < len(get_mail.messages):
-                self.progressbar.setValue(current_number_of_rocessed_list_id := len(get_mail.messages))
-
-                item_from = str(email.header.make_header(email.header.decode_header(get_mail.messages[-1]['from'])))
-                item_subject = str(email.header.make_header(email.header.decode_header(get_mail.messages[-1]['subject'])))
-                self.listLetters.addItem(QtWidgets.QListWidgetItem(f"From : {item_from}\nSubject : {item_subject}"))
-                self.message_from.append(item_from)
-                self.message_subject.append(item_subject)
+    def update_status_bar(self, current: int, max: int):
+        self.progressbar.setMaximum(max)
+        self.progressbar.setValue(current)
+        self.status.setText(f"Mining bitcoin: {current} out of {max}( ‾́ ◡ ‾́ )")
 
     def buttSend_Released(self):
         self.sender = Sender()
